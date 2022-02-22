9d6399a9cbb6bf9c318ced080ae7383f5c7f6a2d
Roxannecvl <Roxanne.chevalley03@gmail.com>
2022-02-22 14:07:51 +0100

    PointCh

--- src/ch/epfl/javelo/projection/PointCh.java
+++ src/ch/epfl/javelo/projection/PointCh.java
@@ -1,4 +1,32 @@
 package ch.epfl.javelo.projection;
 
-public class PointCh {
+import ch.epfl.javelo.Math2;
+
+public record PointCh(double e, double n) {
+    public PointCh{ //constructeur compact
+      if(!SwissBounds.containsEN(e,n)){
+          throw new IllegalArgumentException();
+      }
+    }
+
+   public double squaredDistanceTo(PointCh that){
+       return Math2.squaredNorm(that.e()-this.e(),that.n()-this.n());
+    }
+
+   public double distanceTo(PointCh that){
+        return Math2.norm(that.e()-this.e(),that.n()-this.n());
+   }
+
+   public double lon(){
+        return Ch1903.lon(e(),n());
+   }
+
+   public double lat(){
+        return Ch1903.lat(e(),n());
+   }
+
+
+
+
+
 }
