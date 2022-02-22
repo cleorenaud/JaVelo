f3fecfd31be4f0ef29896417e459cd7a8b4a0056
cleorenaud <cleo.renaud@epfl.ch>
2022-02-22 13:38:15 +0100

    Méthodes ch1903 et swissBounds

--- src/ch/epfl/javelo/Ch1903.java
+++ src/ch/epfl/javelo/Ch1903.java
@@ -1,4 +1,31 @@
 package ch.epfl.javelo;
 
-public class Ch1903 {
+public final class Ch1903 {
+    public static double e(double lon, double lat) {
+        lon = Math.pow(10, -4) * (3600 * lon - 26782.5);
+        lat = Math.pow(10, -4) * (3600 * lat - 169028.66);
+        double eastCoordinate = 2600072.37 + 211455.93 * lon - 10938.51 * lat * lon - 0.36 * lon * Math.pow(lat, 2) - 44.54 * Math.pow(lon, 3);
+        return eastCoordinate;
+    }
+
+    public static double n(double lon, double lat) {
+        lon = Math.pow(10, -4) * (3600 * lon - 26782.5);
+        lat = Math.pow(10, -4) * (3600 * lat - 169028.66);
+        double northCoordinate = 1200147.07 +308807.95 * lat + 3745.25 * Math.pow(lon, 2) + 76.63 * Math.pow(lat, 2) - 194.56 * lat * Math.pow(lon, 2) + 119.79 * Math.pow(lat, 3);
+        return northCoordinate;
+    }
+
+    public static double lon(double e, double n) {
+        e = Math.pow(10, -6) * (e - 2600000);
+        n = Math.pow(10, -6) * (n - 1200000);
+        double longitude = 2.6779094 + 4.728982 * e + 0.791484 * e * n + 0.1306 * e * Math.pow(n, 2) - 0.0436 * Math.pow(e, 3);
+        return longitude;
+    }
+
+    public static double lat(double e, double n) {
+        e = Math.pow(10, -6) * (e - 2600000);
+        n = Math.pow(10, -6) * (n - 1200000);
+        double latitude = 16.9023892 + 3.238272 * n - 0.270978 * Math.pow(e, 2) - 0.002528 * Math.pow(n, 2) - 0.0447 * Math.pow(e, 2) * n - 0.0140 * Math.pow(n, 3);
+        return latitude;
+    }
 }
--- src/ch/epfl/javelo/SwissBounds.java
+++ src/ch/epfl/javelo/SwissBounds.java
@@ -1,4 +1,17 @@
 package ch.epfl.javelo;
 
-public class SwissBounds {
+public final class SwissBounds {
+    public static final double MIN_E = 2485000;
+    public static final double MAX_E = 2834000;
+    public static final double MIN_N = 1075000;
+    public static final double MAX_N = 1296000;
+    public static final double WIDTH = MAX_E - MIN_E;
+    public static final double HEIGHT = MAX_N - MIN_N;
+
+    public static boolean containsEN(double e, double n) {
+        if ((MIN_E < e) || (e < MAX_E) || (MIN_N < n) || (MAX_N > n)) {
+            return true;
+        }
+        else return false;
+    }
 }
