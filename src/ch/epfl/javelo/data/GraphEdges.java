7ad434d6571c0a7e3ae31aea5124ceafc4e317e6
Roxannecvl <Roxanne.chevalley03@gmail.com>
2022-03-02 09:48:07 +0100

    création des trois classes partie 3

---
+++ src/ch/epfl/javelo/data/GraphEdges.java
@@ -0,0 +1,8 @@
+/*
+ * Author : Roxanne Chevalley
+ * Date : 02.03.22
+ */
+package ch.epfl.javelo.data;
+
+public record GraphEdges() {
+}
---
+++ src/ch/epfl/javelo/data/GraphNodes.java
@@ -0,0 +1,11 @@
+/*
+ * Author : Roxanne Chevalley
+ * Date : 02.03.22
+ */
+package ch.epfl.javelo.data;
+
+import java.nio.*;
+
+public record GraphNodes(IntBuffer buffer) {
+
+}
