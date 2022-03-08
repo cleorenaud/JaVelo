fa55f91fe2605a36f1c73e5e0a77855f667383de
cleorenaud <cleo.renaud@epfl.ch>
2022-03-08 13:29:39 +0100

    Enregistrement RoutePoint

--- src/ch/epfl/javelo/routing/RoutePoint.java
+++ src/ch/epfl/javelo/routing/RoutePoint.java
@@ -4,5 +4,55 @@
  */
 package ch.epfl.javelo.routing;
 
-public record RoutePoint() {
+import ch.epfl.javelo.projection.PointCh;
+
+import static java.lang.Double.NaN;
+import static java.lang.Double.POSITIVE_INFINITY;
+
+/**
+ * Enregistrement représentant le point d'un itinéraire le plus proche d'un point de référence donné, qui se trouve dans
+ * le voisinage de l'itinéraire
+ *
+ * @author Cléo Renaud (325156)
+ */
+public record RoutePoint(PointCh point, double position, double distanceToReference) {
+
+    // Constante représentant un point inexistant
+    public static final RoutePoint NONE = new RoutePoint(null, NaN, POSITIVE_INFINITY);
+
+    /**
+     * Méthode retournant un point identique au récepteur (this) mais dont la position est décalée d'une différence
+     * donnée (qui peut être positive ou négative)
+     *
+     * @param positionDifference (double) la différence donnée
+     * @return (RoutePoint) le point décalé
+     */
+    // Méthode destinée à être utilisée pour transformer un point dont la position est exprimée par rapport à un segment
+    // qui le contient en un point équivalent mais dont la position est exprimée par rapport à l'itinéraire complet
+    public RoutePoint withPositionShiftedBy(double positionDifference) {
+      return new RoutePoint(this.point(), this.position() + positionDifference, this.distanceToReference());
+    }
+
+    /**
+     * Méthode retournant this si sa distance à la référence est inférieure ou égale à celle de that, et that sinon
+     *
+     * @param that (RoutePoint) le point passé en argument
+     * @return (RoutePoint) le point le plus proche de la référence
+     */
+    public RoutePoint min(RoutePoint that) {
+        return (this.distanceToReference() <= that.distanceToReference()) ? this : that;
+    }
+
+    /**
+     * Méthode retournant this si sa distance à la référence est inférieure ou égale à thatDistanceToReference, et une
+     * nouvelle instance de RoutePoint dont les attributs sont les arguments passés à min sinon
+     *
+     * @param thatPoint               (PointCh) le point passé en argument
+     * @param thatPosition            (double) la position de thatPoint
+     * @param thatDistanceToReference (double) la distance entre la référence et thatPoint
+     * @return (RoutePoint) le point le plus proche de la référence
+     */
+    public RoutePoint min(PointCh thatPoint, double thatPosition, double thatDistanceToReference) {
+        return (this.distanceToReference() <= thatDistanceToReference) ? this : new RoutePoint(thatPoint, thatPosition, thatDistanceToReference);
+    }
 }
