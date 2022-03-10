23a7bddf23749d819b8327d00739916ce5b6b33d
Roxannecvl <Roxanne.chevalley03@gmail.com>
2022-03-10 13:03:04 +0100

    edge Test

---
+++ test/ch/epfl/javelo/routing/EdgeTest.java
@@ -0,0 +1,13 @@
+package ch.epfl.javelo.routing;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EdgeTest {
+    @Test
+    void constructorTest(){
+        new Edge()
+    }
+
+}
