e05eecf059b5f4e52658a11f1a902bd878770601
cleorenaud <cleo.renaud@epfl.ch>
2022-03-10 14:07:37 +0100

    Test

---
+++ test/ch/epfl/javelo/data/GraphTest.java
@@ -0,0 +1,79 @@
+package ch.epfl.javelo.data;
+
+import ch.epfl.javelo.Math2;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GraphTest {
+
+    /*
+    @Test
+    void loadFromThrowsIOException() {
+        assertThrows(IOException.class, () -> {
+            Graph.loadFrom();
+        });
+    }
+    */
+
+    @Test
+    void loadFromWorksWithKnownInput() {
+
+    }
+
+    @Test
+    void nodeCountWorksOnKnownValues() {
+    }
+
+    @Test
+    void nodePointWorksOnKnownInput() {
+
+    }
+
+    @Test
+    void nodeOutDegreeWorksOnKnownInput() {
+
+    }
+
+    @Test
+    void nodeOutEdgeIdWorksOnKnownInput() {
+
+    }
+
+    @Test
+    void nodeClosestToWorksOnKnownInput() {
+
+    }
+
+    @Test
+    void edgeTargetNodeIdWorksOnKnownInput() {
+
+    }
+
+    @Test
+    void edgeIsInvertedWorksOnKnownInput() {
+
+    }
+
+    @Test
+    void edgeAttributesWorksOnKnownInput() {
+
+    }
+
+    @Test
+    void edgeLengthWorksOnKnownInput() {
+
+    }
+
+    @Test
+    void edgeElevationGainWorksOnKnownInput() {
+
+    }
+
+    @Test
+    void edgeProfileWorksOnKnownInput() {
+
+    }
+}
---
+++ test/ch/epfl/javelo/routing/ElevationProfileTest.java
@@ -0,0 +1,7 @@
+package ch.epfl.javelo.routing;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ElevationProfileTest {
+
+}
