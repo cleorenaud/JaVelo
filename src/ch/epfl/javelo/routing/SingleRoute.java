ca6a967048da655832500b28c3a444b72f51f794
cleorenaud <cleo.renaud@epfl.ch>
2022-03-15 14:29:08 +0100

    Commentaires classe SingleRoute

--- src/ch/epfl/javelo/routing/SingleRoute.java
+++ src/ch/epfl/javelo/routing/SingleRoute.java
@@ -1,4 +1,109 @@
 package ch.epfl.javelo.routing;
 
-public final class SingleRoute {
+import ch.epfl.javelo.projection.PointCh;
+
+import java.util.List;
+
+/**
+ * Classe publique et immuable représentant un itinéraire simple, c'est-à-dire reliant un point de départ à un point
+ * d'arrivée sans point de passage intermédiaire
+ *
+ * @author Cléo Renaud (325156)
+ */
+public final class SingleRoute implements Route {
+    /**
+     * Constructeur retournant l'itinéraire simple composé des arêtes données
+     *
+     * @param edges (List<Edge>) les arêtes données
+     */
+    public SingleRoute(List<Edge> edges) {
+
+    }
+
+    /**
+     * Méthode retournant l'index du segment de l'itinéraire contenant la position donnée,
+     * qui vaut toujours 0 dans le cas d'un itinéraire simple,
+     *
+     * @param position (double) la position donnée (en mètres)
+     * @return (int) l'index du segment de l'itinéraire contenant la position donnée
+     */
+    @Override
+    public int indexOfSegmentAt(double position) {
+        return 0;
+    }
+
+    /**
+     * Méthode retournant la longueur de l'itinéraire
+     *
+     * @return (double) la longueur de l'itinéraire (en mètres)
+     */
+    @Override
+    public double length() {
+        return 0;
+    }
+
+    /**
+     * Méthode retournant la totalité des arêtes de l'itinéraire
+     *
+     * @return (List < Edge >) la liste contenant la totalité des arêtes de l'itinéraire
+     */
+    @Override
+    public List<Edge> edges() {
+        return null;
+    }
+
+    /**
+     * Méthode retournant la totalité des points situés aux extrémités des arêtes de l'itinéraire
+     *
+     * @return (List < Edge >) la liste contenant la totalité des points situés aux extrémités des arêtes de l'itinéraire
+     */
+    @Override
+    public List<PointCh> points() {
+        return null;
+    }
+
+    /**
+     * Méthode retournant le point se trouvant à la position donnée le long de l'itinéraire
+     *
+     * @param position (double) la position donnée le long de l'itinéraire
+     * @return (PointCh) le point correspondant à la position donnée
+     */
+    @Override
+    public PointCh pointAt(double position) {
+        return null;
+    }
+
+    /**
+     * Méthode retournant l'altitude à la position donnée le long de l'itinéraire,
+     * qui peut valoir NaN si l'arête contenant cette position n'a pas de profil
+     *
+     * @param position (double) la position donnée le long de l'itinéraire
+     * @return (double) l'altitude à la position donnée le long de l'itinéraire
+     */
+    @Override
+    public double elevationAt(double position) {
+        return 0;
+    }
+
+    /**
+     * Méthode retournant l'identité du nœud appartenant à l'itinéraire et se trouvant le plus proche de la position donnée
+     *
+     * @param position (double) la position donnée
+     * @return (int) l'identité du nœud le plus proche de la position donnée
+     */
+    @Override
+    public int nodeClosestTo(double position) {
+        return 0;
+    }
+
+    /**
+     * Méthode retournant le point de l'itinéraire se trouvant le plus proche du point de référence donné
+     *
+     * @param point (PointCh) le point de référence
+     * @return (RoutePoint) le point de l'itinéraire la plus proche du point de référence
+     */
+    @Override
+    public RoutePoint pointClosestTo(PointCh point) {
+        return null;
+    }
 }
