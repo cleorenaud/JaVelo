b727631cfd396713947b7055db61366f939717ac
cleorenaud <cleo.renaud@epfl.ch>
2022-03-17 12:41:57 +0100

    Commit

--- test/ch/epfl/javelo/data/GraphTest.java
+++ test/ch/epfl/javelo/data/GraphTest.java
@@ -24,14 +24,14 @@ class GraphTest {
 
 
     @Test
-    //DONE
+        //DONE
     void loadFromDoesNotThrowsIOException() throws IOException {
         Graph graph = Graph.loadFrom(Path.of("Lausanne"));
 
     }
 
     @Test
-    //DONE
+        //DONE
     void loadFromThrowsIOException() {
         assertThrows(IOException.class, () -> {
             Graph graph = Graph.loadFrom(Path.of("src"));
@@ -39,7 +39,7 @@ class GraphTest {
     }
 
     @Test
-    //DONE
+        //DONE
     void nodeCountWorksOnKnownValues() throws IOException {
         Path filePath = Path.of("lausanne");
         Graph graph = Graph.loadFrom(filePath);
@@ -55,7 +55,7 @@ class GraphTest {
     }
 
     @Test
-    //DONE
+        //DONE
     void nodePointWorksOnKnownInput() throws IOException {
         Graph graph = Graph.loadFrom(Path.of("Lausanne"));
         double e = Ch1903.e(Math.toRadians(6.6013034), Math.toRadians(46.6326106));
@@ -95,7 +95,7 @@ class GraphTest {
     }
 
     @Test
-    //DONE
+        //DONE
     void nodeClosestToWorksOnKnownInput() throws IOException {
         Graph graph = Graph.loadFrom(Path.of("Lausanne"));
         double e = Ch1903.e(Math.toRadians(6.601303), Math.toRadians(46.632610)); // presque les memes coordonnées que la node 2022
@@ -106,38 +106,76 @@ class GraphTest {
 
 
     @Test
-    //TO DO
+        //TO DO
     void edgeTargetNodeIdWorksOnKnownInput() throws IOException {
 
     }
 
     @Test
-    //TO DO
+        //TO DO
     void edgeIsInvertedWorksOnKnownInput() {
 
     }
 
     @Test
-    //TO DO
+        //TO DO
     void edgeAttributesWorksOnKnownInput() {
 
     }
 
     @Test
-    //TO DO
+        //TO DO
     void edgeLengthWorksOnKnownInput() {
 
     }
 
     @Test
-    //TO DO
+        //TO DO
     void edgeElevationGainWorksOnKnownInput() {
 
     }
 
     @Test
-    //TO DO
-    void edgeProfileWorksOnKnownInput() {
+        //TO DO
+    void edgeProfileWorksOnKnownInput() throws IOException {
+        //Buffer pour les nodes
+        IntBuffer b = IntBuffer.wrap(new int[]{
+                2_600_000 << 4,
+                1_200_000 << 4,
+                0x2_000_1234
+        });
+        GraphNodes gn = new GraphNodes(b);
 
+        //Buffers pour les edges
+        ByteBuffer edgesBuffer = ByteBuffer.allocate(10);
+        IntBuffer profileIds = IntBuffer.wrap(new int[]{
+                // Type : 3. Index du premier échantillon : 1.
+                (3 << 30) | 1
+        });
+        ShortBuffer elevations = ShortBuffer.wrap(new short[]{
+                (short) 0,
+                (short) 0x180C, (short) 0xFEFF,
+                (short) 0xFFFE, (short) 0xF000
+        });
+        GraphEdges ge = new GraphEdges(edgesBuffer, profileIds, elevations);
+
+        //Buffer pour le secteur
+        ByteBuffer buffer1 = ByteBuffer.wrap(new byte[]{0b000,
+                0b000, 0b000, 0b001, 0b000, 0b001
+
+        });
+        GraphSectors gs = new GraphSectors(buffer1);
+
+        //Liste d'attributs
+        List<AttributeSet> attributeSet = new ArrayList<>();
+        Graph testGraph = new Graph(gn, gs, ge, attributeSet);
+
+        Graph graph = Graph.loadFrom(Path.of("Lausanne"));
+
+        System.out.println(graph.edgeProfile(7).applyAsDouble(1));
+        System.out.println(graph.edgeProfile(7).applyAsDouble(2));
+        System.out.println(graph.edgeProfile(7).applyAsDouble(3));
+        System.out.println(graph.edgeProfile(7).applyAsDouble(30));
+        ;
     }
 }
