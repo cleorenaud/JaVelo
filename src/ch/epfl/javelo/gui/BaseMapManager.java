0afe1fb84c8ace1b434091f0c40cd671f15f57bd
cleorenaud <cleo.renaud@epfl.ch>
2022-04-12 09:08:26 +0200

    Update BaseMapManager.java

--- src/ch/epfl/javelo/gui/BaseMapManager.java
+++ src/ch/epfl/javelo/gui/BaseMapManager.java
@@ -1,5 +1,8 @@
 package ch.epfl.javelo.gui;
 
+import javafx.beans.property.ObjectProperty;
+import javafx.scene.layout.Pane;
+
 /**
  * Classe publique et finale qui gère l'affichage et interaction avec le fond de carte
  */
@@ -10,9 +13,10 @@ public final class BaseMapManager {
      *
      * @param tileManager (TileManager) : le gestionnaire de tuiles à utiliser pour obtenir les tuiles de la carte
      * @param waypointsManager (WaypointsManager) : le gestionnaire des points de passage
+     * @param objectProperty (ObjectProperty) : une propriété JavaFX contenant les paramètres de la carte affichée
      */
-    public BaseMapManager(TileManager tileManager, WaypointsManager waypointsManager) {
-
+    public Pane BaseMapManager(TileManager tileManager, WaypointsManager waypointsManager, ObjectProperty objectProperty) {
+        
     }
 
 
