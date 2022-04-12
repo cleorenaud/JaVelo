c1cf6c039a9dd86b4e11eba82652233986db2bc4
Roxannecvl <Roxanne.chevalley03@gmail.com>
2022-04-12 10:01:12 +0200

    WaysPointManager

--- src/ch/epfl/javelo/gui/WaypointsManager.java
+++ src/ch/epfl/javelo/gui/WaypointsManager.java
@@ -1,18 +1,60 @@
 package ch.epfl.javelo.gui;
 
+import ch.epfl.javelo.data.Graph;
+import ch.epfl.javelo.projection.PointCh;
+import ch.epfl.javelo.projection.PointWebMercator;
+import javafx.beans.property.ObjectProperty;
+import javafx.scene.Group;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.SVGPath;
+
+import java.util.List;
+
 /**
  * Classe publique et finale qui gère l'affichage et l'interaction avec les points de passage
  */
 public final class WaypointsManager {
-
+private final Graph graph;
+private final ObjectProperty objectProperty;
+private final List<Waypoint> pointDePassage;
     /**
      * Constructeur public de la classe
      */
-    public WaypointsManager() {
-
+    public WaypointsManager(Graph graph, ObjectProperty objectProperty, List<Waypoint> pointDePassage) {
+        this.graph =graph;
+        this.objectProperty= objectProperty;
+        this.pointDePassage = pointDePassage;
     }
 
-    public void pane() {
+    public Pane pane() {
+        Pane carte = new Pane();
+        SVGPath fils1 = new SVGPath();
+        fils1.setContent("M-8-20C-5-14-2-7 0 0 2-7 5-14 8-20 20-40-20-40-8-20");
+        fils1.getStyleClass().add("pin_outside");
+        SVGPath fils2 = new SVGPath();
+        fils2.getStyleClass().add("pin_inside");
+        fils2.setContent("M0-23A1 1 0 000-29 1 1 0 000-23");
+
+        for (int i = 0; i < pointDePassage.size(); i++) {
+           Group marqueur = new Group(fils1, fils2);
+           marqueur.getStyleClass().add("pin");
+           if(i==0){
+               marqueur.getStyleClass().add("first");
+           }else if (i==pointDePassage.size()-1){
+               marqueur.getStyleClass().add("last");
+           }else{
+               marqueur.getStyleClass().add("middle");
+           }
+
+
+
+
+
+           PointCh pointCh=  pointDePassage.get(i).point();
+           PointWebMercator webMercator = PointWebMercator.ofPointCh(pointCh);
+
+        }
+        return null;
 
     }
 
