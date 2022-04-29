6958c467d2150294745d2c35642b4294ed571a69
cleorenaud <cleo.renaud@epfl.ch>
2022-04-29 13:49:21 +0200

    Correction

--- src/ch/epfl/javelo/Preconditions.java
+++ src/ch/epfl/javelo/Preconditions.java
@@ -10,13 +10,15 @@ public final class Preconditions {
     /**
      * Constructeur privé pour que la classe ne soit pas instantiable
      */
-    private Preconditions() {}
+    private Preconditions() {
+    }
 
     /**
      * Méthode permettant de lever l'exception IllegalArgumentException si son argument est faux
      * et ne fait rien sinon
      *
      * @param shouldBeTrue (boolean) : l'expression passée en argument qui doit être vérifiée
+     * @throws IllegalArgumentException : si l'argument passé en paramètre est faux
      */
     public static void checkArgument(boolean shouldBeTrue) {
         if (!shouldBeTrue) {
