a24354afa8d2907a76469e5ed93b8a4da4aff6ed
cleorenaud <cleo.renaud@epfl.ch>
2022-05-04 14:57:16 +0200

    Etape 10

--- src/ch/epfl/javelo/gui/ElevationProfileManager.java
+++ src/ch/epfl/javelo/gui/ElevationProfileManager.java
@@ -3,6 +3,7 @@ package ch.epfl.javelo.gui;
 import ch.epfl.javelo.routing.ElevationProfile;
 import javafx.beans.property.*;
 import javafx.geometry.Insets;
+import javafx.geometry.Point2D;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Group;
 import javafx.scene.layout.BorderPane;
@@ -145,43 +146,49 @@ public final class ElevationProfileManager {
     }
 
     private void drawLines() {
-       path.getElements().clear(); // Nœud représentant la totalité des lignes verticale et horizontales composant la grille du profil
+        path.getElements().clear(); // Nœud représentant la totalité des lignes verticale et horizontales composant la grille du profil
 
         double rectWidth = pane.getWidth() - rectInsets.getRight() - rectInsets.getLeft();
         double rectHeight = pane.getHeight() - rectInsets.getTop() - rectInsets.getBottom();
 
         // On itère sur POS_STEPS pour déterminer quel est l'espacement entre les lignes verticales
-        double verticalSpacing = 0;
+        double vertLinesSpacing = 0;
         for (int i : POS_STEPS) {
-            verticalSpacing = (length * 1000) / i; // On convertit la longueur en kilomètres
-            //verticalSpacing = worldToScreen.deltaTransform(0, verticalSpacing); // On transforme la valeur obtenue
-            if (verticalSpacing >= 25) {
+            // On calcule la distance entre chaque ligne horizontale pour chaque valeur de POS_STEPS dans le "monde réel"
+            // puis on la convertit dans le système de coordonnées JavaFX
+            Point2D vertInterval = worldToScreen.get().deltaTransform(length / i, 0);
+            // Si l'intervalle entre deux lignes verticales est supérieur ou égal à 25 on a notre valeur de POS_STEPS à utiliser
+            if (vertInterval.getX() >= 25) {
                 break;
             }
+            vertLinesSpacing = vertInterval.getX();
         }
-        double numVLines = Math.floor(rectWidth / verticalSpacing);
+        double numVLines = Math.floor(rectWidth / vertLinesSpacing);
 
         for (int i = 0; i < numVLines; i++) {
             path.getElements().add(
-                    new MoveTo(rectInsets.getLeft() + (i * verticalSpacing), rectInsets.getTop()));
+                    new MoveTo(rectInsets.getLeft() + (i * vertLinesSpacing), rectInsets.getTop()));
             path.getElements().add(
                     new LineTo(rectInsets.getLeft(), rectInsets.getTop() + rectHeight));
         }
 
         // On itère sur ELE_STEPS pour déterminer quel est l'espacement entre les lignes horizontales
-        double horizontalSpacing = 0;
+        double horLinesSpacing = 0;
         for (int i : ELE_STEPS) {
-            horizontalSpacing = (maxElevation - minElevation) * 1000 / i; // On convertit l'altitude en kilomètres
-            horizontalSpacing = worldToScreen.deltaTransform(0, horizontalSpacing); // On transforme la valeur obtenue
-            if (horizontalSpacing >= 50) {
+            // On calcule la distance entre chaque ligne verticale pour chaque valeur de ELE_STEPS dans le "monde réel"
+            // puis on la convertit dans le système de coordonnées JavaFX
+            Point2D horInterval = worldToScreen.get().deltaTransform(0, (maxElevation - minElevation) / i);
+            // Si l'intervalle entre deux lignes horizontales est supérieur ou égal à 50 on a notre valeur de ELE_STEPS à utiliser
+            if (horInterval.getY() >= 50) {
                 break;
             }
+            horLinesSpacing = horInterval.getY();
         }
-        double numHLines = Math.floor(rectHeight / horizontalSpacing);
+        double numHLines = Math.floor(rectHeight / horLinesSpacing);
 
         for (int i = 0; i < numHLines; i++) {
             path.getElements().add(
-                    new MoveTo(rectInsets.getLeft(), rectInsets.getTop() + rectHeight - (i * horizontalSpacing)));
+                    new MoveTo(rectInsets.getLeft(), rectInsets.getTop() + rectHeight - (i * horLinesSpacing)));
             path.getElements().add(
                     new LineTo(rectInsets.getLeft() + rectWidth, rectInsets.getTop() + rectHeight));
         }
