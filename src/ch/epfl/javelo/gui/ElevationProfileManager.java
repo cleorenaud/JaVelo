7f466ba5879ecd7109a4806b2b33f5f8d5aa7437
cleorenaud <cleo.renaud@epfl.ch>
2022-05-04 13:59:13 +0200

    Etape 10

--- src/ch/epfl/javelo/gui/ElevationProfileManager.java
+++ src/ch/epfl/javelo/gui/ElevationProfileManager.java
@@ -3,15 +3,12 @@ package ch.epfl.javelo.gui;
 import ch.epfl.javelo.routing.ElevationProfile;
 import javafx.beans.property.*;
 import javafx.geometry.Insets;
-import javafx.geometry.Point2D;
 import javafx.geometry.Rectangle2D;
 import javafx.scene.Group;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
-import javafx.scene.shape.Line;
-import javafx.scene.shape.Path;
-import javafx.scene.shape.Polygon;
+import javafx.scene.shape.*;
 import javafx.scene.text.Text;
 import javafx.scene.transform.Affine;
 import javafx.scene.transform.NonInvertibleTransformException;
@@ -52,40 +49,39 @@ public final class ElevationProfileManager {
     private Transform screenToWorld;
     private Transform worldToScreen;
 
-    private final static Insets edgeDistances = new Insets(10, 10, 20, 40);
-    private final static int[] POS_STEPS =
-            { 1000, 2000, 5000, 10_000, 25_000, 50_000, 100_000 };
-    private final static int[] ELE_STEPS =
-            { 5, 10, 20, 25, 50, 100, 200, 250, 500, 1_000 };
+    private final static Insets rectInsets = new Insets(10, 10, 20, 40);
+    private final static int[] POS_STEPS = {1000, 2000, 5000, 10_000, 25_000, 50_000, 100_000};
+    private final static int[] ELE_STEPS = {5, 10, 20, 25, 50, 100, 200, 250, 500, 1_000};
 
     /**
      * Constructeur public de la classe
+     *
      * @param elevationProfileProperty (ReadOnlyObjectProperty<ElevationProfile>) : une propriété contenant
      *                                 le profil à afficher
-     * @param position (ReadOnlyDoubleProperty) : une propriété contenant
-     *                 la position le long du profil à mettre en évidence
+     * @param position                 (ReadOnlyDoubleProperty) : une propriété contenant
+     *                                 la position le long du profil à mettre en évidence
      */
     public ElevationProfileManager(ReadOnlyObjectProperty<ElevationProfile> elevationProfileProperty,
                                    ReadOnlyDoubleProperty position) {
         this.position = position;
         this.elevationProfileProperty = elevationProfileProperty;
-        this.mousePosition= new SimpleDoubleProperty(NaN);
-        this.profileRect= new SimpleObjectProperty<>();
-        this.transformObjectProperty= new SimpleObjectProperty<>();
+        this.mousePosition = new SimpleDoubleProperty(NaN);
+        this.profileRect = new SimpleObjectProperty<>();
+        this.transformObjectProperty = new SimpleObjectProperty<>();
 
         this.textBottom = new Text();
-        this.vBox= new VBox(textBottom);
+        this.vBox = new VBox(textBottom);
         vBox.setId("profile_data");
 
         this.line = new Line();
-        this.polygon= new Polygon();
+        this.polygon = new Polygon();
         this.polygon.setId("profile");
         this.groupForText = new Group();
-        this.path= new Path();
+        this.path = new Path();
         this.path.setId("grid");
-        this.pane= new Pane(path,groupForText,polygon,line);
+        this.pane = new Pane(path, groupForText, polygon, line);
 
-        this.borderPane= new BorderPane(pane);
+        this.borderPane = new BorderPane(pane);
         borderPane.setBottom(vBox);
         borderPane.getStylesheets().add("elevation_profile.css");
 
@@ -116,14 +112,14 @@ public final class ElevationProfileManager {
 
 
     /**
-     * Méthode permettant de créer les tranformations
+     * Méthode permettant de créer les transformations
      */
     private void setTransforms() {
-        double slopeX= length / paneWidth;
-        double bX = - 40 * slopeX;
-        double slopeY = -(maxElevation-minElevation)/paneHeight;
-        double bY= 10 * slopeY + maxElevation;
-        screenToWorld= new Affine(slopeX, 0, bX, 0, slopeY, bY);
+        double slopeX = length / paneWidth;
+        double bX = -40 * slopeX;
+        double slopeY = -(maxElevation - minElevation) / paneHeight;
+        double bY = 10 * slopeY + maxElevation;
+        screenToWorld = new Affine(slopeX, 0, bX, 0, slopeY, bY);
 
         try {
             worldToScreen = screenToWorld.createInverse();
@@ -133,12 +129,12 @@ public final class ElevationProfileManager {
 
     }
 
-    private void redraw(){
+    private void redraw() {
         maxElevation = elevationProfileProperty.get().maxElevation();
         minElevation = elevationProfileProperty.get().minElevation();
         length = elevationProfileProperty.get().length();
-        paneWidth= pane.getWidth()-50;
-        paneHeight= pane.getHeight()-30;
+        paneWidth = pane.getWidth() - 50;
+        paneHeight = pane.getHeight() - 30;
 
         setTransforms();
 
@@ -146,37 +142,75 @@ public final class ElevationProfileManager {
         // TODO : cette méthode devra appeler drawLines, drawProfile et writeText
     }
 
-    private void drawLines(){
+    private void drawLines() {
+        Path lines = new Path(); // Nœud représentant la totalité des lignes verticale et horizontales composant la grille du profil
 
+        double rectWidth = pane.getWidth() - rectInsets.getRight() - rectInsets.getLeft();
+        double rectHeight = pane.getHeight() - rectInsets.getTop() - rectInsets.getBottom();
 
+        // On itère sur POS_STEPS pour déterminer quel est l'espacement entre les lignes verticales
+        double verticalSpacing = 0;
+        for (int i : POS_STEPS) {
+            verticalSpacing = (length * 1000) / i; // On convertit la longueur en kilomètres
+            //verticalSpacing = worldToScreen.deltaTransform(0, verticalSpacing); // On transforme la valeur obtenue
+            if (verticalSpacing >= 25) {
+                break;
+            }
+        }
+        double numVLines = Math.floor(rectWidth / verticalSpacing);
 
+        for (int i = 0; i < numVLines; i++) {
+            lines.getElements().add(
+                    new MoveTo(rectInsets.getLeft() + (i * verticalSpacing), rectInsets.getTop()));
+            lines.getElements().add(
+                    new LineTo(rectInsets.getLeft(), rectInsets.getTop() + rectHeight));
+        }
+
+        // On itère sur ELE_STEPS pour déterminer quel est l'espacement entre les lignes horizontales
+        double horizontalSpacing = 0;
+        for (int i : ELE_STEPS) {
+            horizontalSpacing = (maxElevation - minElevation) * 1000 / i; // On convertit l'altitude en kilomètres
+            // horizontalSpacing = worldToScreen.deltaTransform(0, horizontalSpacing); // On transforme la valeur obtenue
+            if (horizontalSpacing >= 50) {
+                break;
+            }
+        }
+        double numHLines = Math.floor(rectHeight / horizontalSpacing);
+
+        for (int i = 0; i < numHLines; i++) {
+            lines.getElements().add(
+                    new MoveTo(rectInsets.getLeft(), rectInsets.getTop() + rectHeight - (i * horizontalSpacing)));
+            lines.getElements().add(
+                    new LineTo(rectInsets.getLeft() + rectWidth, rectInsets.getTop() + rectHeight));
+        }
 
     }
 
-    private void drawProfile(){
+    private void drawProfile() {
 
     }
 
-    private void writeText(){
+    private void writeText() {
         double totalDescent = elevationProfileProperty.get().totalDescent();
-        double totalAscent =  elevationProfileProperty.get().totalAscent();
+        double totalAscent = elevationProfileProperty.get().totalAscent();
         String text = String.format("Longueur : %.1f km" +
-                "     Montée : %.0f m" +
-                "     Descente : %.0f m" +
-                "     Altitude : de %.0f m à %.0f m",
-                length/1000.0, totalAscent, totalDescent, minElevation,maxElevation
+                        "     Montée : %.0f m" +
+                        "     Descente : %.0f m" +
+                        "     Altitude : de %.0f m à %.0f m",
+                length / 1000.0, totalAscent, totalDescent, minElevation, maxElevation
         );
 
         textBottom.setText(text);
     }
 
-    private void installHandlers(){
+    private void installHandlers() {
 
     }
+
     private void innerRectangle() {
         Pane innerRectangle = new Pane();
-        double prefWidth = pane.getWidth() - edgeDistances.getLeft() - edgeDistances.getRight();
-        double prefHeight = pane.getHeight() - edgeDistances.getTop() - edgeDistances.getBottom();
+        double prefWidth = pane.getWidth() - rectInsets.getLeft() - rectInsets.getRight();
+        double prefHeight = pane.getHeight() - rectInsets.getTop() - rectInsets.getBottom();
         innerRectangle.setPrefSize(prefWidth, prefHeight);
     }
 }
